1ae3b6b76d10d566391d2d405dc090cc25d87a38
sso66 <sun.stephen.oo@gmail.com>
2020-05-07 07:35:58 -0400

    First commit

---
+++ src/components/VideoReact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Player } from 'video-react';
+
+export default props => {
+  return (
+    <Player
+      className='player'
+      playsInline
+      poster="/assets/poster.png"
+      src="https://media.w3.org/2010/05/sintel/trailer_hd.mp4"
+    />
+  );
+};
