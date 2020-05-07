f3f40a2b8546129123c56341fc8ef745678faf71
sso66 <sun.stephen.oo@gmail.com>
2020-05-06 21:59:25 -0400

    Add new file

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+// AppModel.jsx
+import React, { Component } from 'react';
+import Modal from './components/dialogs/Modal';
+import VideoReact from './components/VideoReact';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { isOpen: false };
+  }
+
+  toggleModal = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <button onClick={this.toggleModal}>
+          Open the modal
+        </button>
+
+        <Modal show={this.state.isOpen}
+          onClose={this.toggleModal}>
+            <VideoReact />
+        </Modal>
+
+      </div>
+    );
+  }
+}
+
+export default App;
+
+// eof
